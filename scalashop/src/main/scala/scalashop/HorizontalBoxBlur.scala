9b503b0b13578dbabb75ec6c9feef212c766d1f4
reinno <reinno@126.com>
2016-06-05 09:26:34 +0800

    scalashop fin

---
+++ scalashop/src/main/scala/scalashop/HorizontalBoxBlur.scala
@@ -0,0 +1,74 @@
+package scalashop
+
+import org.scalameter._
+import common._
+
+object HorizontalBoxBlurRunner {
+
+  val standardConfig = config(
+    Key.exec.minWarmupRuns -> 5,
+    Key.exec.maxWarmupRuns -> 10,
+    Key.exec.benchRuns -> 10,
+    Key.verbose -> true
+  ) withWarmer (new Warmer.Default)
+
+  def main(args: Array[String]): Unit = {
+    val radius = 3
+    val width = 1920
+    val height = 1080
+    val src = new Img(width, height)
+    val dst = new Img(width, height)
+    val seqtime = standardConfig measure {
+      HorizontalBoxBlur.blur(src, dst, 0, height, radius)
+    }
+    println(s"sequential blur time: $seqtime ms")
+
+    val numTasks = 32
+    val partime = standardConfig measure {
+      HorizontalBoxBlur.parBlur(src, dst, numTasks, radius)
+    }
+    println(s"fork/join blur time: $partime ms")
+    println(s"speedup: ${seqtime / partime}")
+  }
+}
+
+
+/** A simple, trivially parallelizable computation. */
+object HorizontalBoxBlur {
+
+  /** Blurs the rows of the source image `src` into the destination image `dst`,
+    * starting with `from` and ending with `end` (non-inclusive).
+    *
+    * Within each row, `blur` traverses the pixels by going from left to right.
+    */
+  def blur(src: Img, dst: Img, from: Int, end: Int, radius: Int): Unit = {
+    // TODO implement this method using the `boxBlurKernel` method
+    for {
+      x <- 0 until src.width
+      y <- from until end
+    } dst.update(x, y, boxBlurKernel(src, x, y, radius))
+  }
+
+  /** Blurs the rows of the source image in parallel using `numTasks` tasks.
+    *
+    * Parallelization is done by stripping the source image `src` into
+    * `numTasks` separate strips, where each strip is composed of some number of
+    * rows.
+    */
+  def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit = {
+    // TODO implement using the `task` construct and the `blur` method
+    val min = src.height / numTasks + 1
+
+    val sliding = min match {
+      case 1 =>
+        (0 until src.height).map(x => List(x, x + 1))
+      case _ =>
+        (0 to src.height).sliding(min, min - 1).toList
+    }
+
+    val tasks = sliding.map(y => task(blur(src, dst, y.head, y.last, radius)))
+
+    tasks.foreach(_.join)
+  }
+
+}
